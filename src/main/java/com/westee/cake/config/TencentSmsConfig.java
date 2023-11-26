0b7c8061bd947ea4486bf61d898b7eb8dcdfbba6
dong <904885508@qq.com>
2023-11-26 16:13:40 +0800

    add tencent sms draft

---
+++ src/main/java/com/westee/cake/config/TencentSmsConfig.java
@@ -0,0 +1,47 @@
+package com.westee.cake.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config.properties") //读取配置文件
+@ConfigurationProperties(prefix = "sms")
+public class TencentSmsConfig {
+    static String secretId;
+    public static String secretKey;
+    public static String sdkAppId;
+    public static String templateId;
+
+    public static String getSecretId() {
+        return TencentSmsConfig.secretId;
+    }
+
+    public void setSecretId(String secretId) {
+        TencentSmsConfig.secretId = secretId;
+    }
+
+    public static String getSecretKey() {
+        return TencentSmsConfig.secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        TencentSmsConfig.secretKey = secretKey;
+    }
+
+    public static String getSdkAppId() {
+        return TencentSmsConfig.sdkAppId;
+    }
+
+    public void setSdkAppId(String sdkAppId) {
+        TencentSmsConfig.sdkAppId = sdkAppId;
+    }
+
+    public static String getTemplateId() {
+        return TencentSmsConfig.templateId;
+    }
+
+    public void setTemplateId(String templateId) {
+        TencentSmsConfig.templateId = templateId;
+    }
+}
--- src/main/java/com/westee/cake/service/SmsCodeService.java
+++ src/main/java/com/westee/cake/service/SmsCodeService.java
@@ -2,4 +2,6 @@ package com.westee.cake.service;
 
 public interface SmsCodeService {
     String sendCode(String tel);
+
+    boolean getCodeStatus(String tel, String code);
 }
